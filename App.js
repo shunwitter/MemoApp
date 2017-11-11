5eff45aa960219d6a6466a2e1388e292471e6a37
shunwitter <polytechnics.shun@gmail.com>
2017-11-11 22:37:55 +0900

    Create MemoEditScreen

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoDetailScreen />
+        <MemoEditScreen />
       </View>
     );
   }
---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+
+import CircleButton from '../elements/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.memoEditInput} multiline value="Hi" />
+        <CircleButton>
+          {'\uf00c'}
+        </CircleButton>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  memoEditInput: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+    fontSize: 16,
+  },
+});
+
+export default MemoEditScreen;
